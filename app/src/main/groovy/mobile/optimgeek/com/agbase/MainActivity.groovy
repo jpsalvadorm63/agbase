e9097d835bdd33a41707db6460a56923ec304d8c
salvadorjp63 <jpsalvadorm@briza-soft.com>
2016-04-22 16:55:26 -0500

    Better structure for MainActivity

--- app/src/main/groovy/mobile/optimgeek/com/agbase/MainActivity.groovy
+++ app/src/main/groovy/mobile/optimgeek/com/agbase/MainActivity.groovy
@@ -1,11 +1,17 @@
 package mobile.optimgeek.com.agbase
 
 import android.app.Activity
+import android.content.Intent
+
 // import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import groovy.transform.CompileStatic
 
+
+@CompileStatic
 public class MainActivity extends Activity {
 
     EditText     user
@@ -19,11 +25,12 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        controls2fields()
-        data2controls()
+        myControls()
+        myData()
+        myEvents()
     }
 
-    protected void controls2fields() {
+    protected void myControls() {
         user      = findViewById(R.id.txtUser) as EditText
         password  = findViewById(R.id.txtPassword) as EditText
         btnSubmit = findViewById(R.id.btnSubmit) as Button
@@ -31,11 +38,28 @@ public class MainActivity extends Activity {
         loginAct  = this
     }
 
-    protected void data2controls() {
+    protected void myData() {
         user.text = "fic"
         password.text = 'fic2015'
     }
 
+    protected void myEvents() {
 
+        btnSubmit.onClickListener =  {
+            if(user.getText().toString().equals("fic") && password.getText().toString().equals("fic2015")) {
+                Toast.makeText(this, ". . . in construction !", Toast.LENGTH_SHORT).show()
+                /*
+                Intent inent = new Intent(loginAct, mobile.sigtierras.gob.ec.fic.MainMenuActivity.class);
+                startActivity(inent);
+                */
+            }
+        }
+
+        btnExit.onClickListener = {
+            Toast.makeText(this, "Hasta la próxima !,  pórtate bien . . .", Toast.LENGTH_SHORT).show()
+            finish();
+        }
+
+    }
 
 }
