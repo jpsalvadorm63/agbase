5e2eb270a35c560e6c57668649174e1e3936f601
salvadorjp63 <jpsalvadorm@briza-soft.com>
2016-04-23 08:01:33 -0500

    TicTacToecreated

---
+++ app/src/main/groovy/mobile/optimgeek/com/agbase/T3Activity.groovy
@@ -0,0 +1,13 @@
+package mobile.optimgeek.com.agbase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class T3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_t3);
+    }
+}
